6b052796c1f349aa30fecd1e56dcff3164e84fd1
kelvinpompey <kelvin.pompey@gmail.com>
2024-10-22 23:41:37 -0400

    Test otp

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { ImageBackground, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 import { observer, Show } from '@legendapp/state/react';
 
@@ -6,12 +6,19 @@ import { Button } from '~/components/Button';
 import { useRef } from 'react';
 import { questionStore$ } from '~/store/question';
 import { Question } from '~/components/Question';
+import { pb } from '~/lib/pb';
 
 function Home() {
   const renderCount = useRef(1).current++;
 
+  const handleLogin = () => {
+    pb.collection('users').requestOTP('kelvin.pompey@gmail.com').then(console.log);
+  };
+
   return (
     <View className="flex flex-1 items-center justify-center gap-6 bg-[#0066cc]">
+      <Button title="Login" onPress={handleLogin}></Button>
+
       <Show if={questionStore$.answerState.get() === 'correct'}>
         <Text className="font-white animate-pulse text-3xl font-bold text-yellow-400 duration-2000">
           Correct!
